d9caacd57566c86f805307d02560b77c991fd94c
chriopter <82179548+chriopter@users.noreply.github.com>
2026-04-04 16:02:21 +0200

    update ruby

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_01_183000) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_04_140106) do
   create_table "entries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.json "facts", default: [], null: false
