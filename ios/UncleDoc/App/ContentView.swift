52d32dd93304ec1a1235e60788d4ac483656aad0
chriopter <82179548+chriopter@users.noreply.github.com>
2026-04-06 12:28:33 +0200

    adjust hello world ios

--- ios/UncleDoc/App/ContentView.swift
+++ ios/UncleDoc/App/ContentView.swift
@@ -4,6 +4,9 @@ struct ContentView: View {
     var body: some View {
         Text("Hello World")
             .font(.largeTitle)
+            .foregroundStyle(.black)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.yellow)
     }
 }
 
