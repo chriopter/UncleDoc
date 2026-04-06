b85f0af42b6dd6aad6ceed71b14733ba210487a8
chriopter <82179548+chriopter@users.noreply.github.com>
2026-04-06 18:39:35 +0200

    add backfill

--- db/migrate/20260406184000_add_uuid_to_people.rb
+++ db/migrate/20260406184000_add_uuid_to_people.rb
@@ -2,5 +2,20 @@ class AddUuidToPeople < ActiveRecord::Migration[8.0]
   def change
     add_column :people, :uuid, :string
     add_index :people, :uuid, unique: true
+
+    reversible do |dir|
+      dir.up do
+        say_with_time "Backfilling people UUIDs" do
+          Person.reset_column_information
+          Person.find_each do |person|
+            person.update_columns(uuid: SecureRandom.uuid) if person.uuid.blank?
+          end
+        end
+      end
+    end
+  end
+
+  class Person < ApplicationRecord
+    self.table_name = "people"
   end
 end
