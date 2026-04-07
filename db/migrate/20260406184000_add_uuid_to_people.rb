806b1a4c55745a403be57d231e2b3707bb033e8f
chriopter <82179548+chriopter@users.noreply.github.com>
2026-04-07 12:58:22 +0200

    adjust mnigrations

--- db/migrate/20260406184000_add_uuid_to_people.rb
+++ db/migrate/20260406184000_add_uuid_to_people.rb
@@ -1,21 +1,44 @@
 class AddUuidToPeople < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def up
-    add_column :people, :uuid, :string
+    with_people_fk_suspended do
+      add_column :people, :uuid, :string
 
-    say_with_time "Backfilling people UUIDs" do
-      Person.reset_column_information
-      Person.find_each do |person|
-        person.update_columns(uuid: SecureRandom.uuid) if person.uuid.blank?
+      say_with_time "Backfilling people UUIDs" do
+        Person.reset_column_information
+        Person.find_each do |person|
+          person.update_columns(uuid: SecureRandom.uuid) if person.uuid.blank?
+        end
       end
-    end
 
-    change_column_null :people, :uuid, false
-    add_index :people, :uuid, unique: true
+      change_column_null :people, :uuid, false
+      add_index :people, :uuid, unique: true
+    end
   end
 
   def down
-    remove_index :people, :uuid
-    remove_column :people, :uuid
+    with_people_fk_suspended do
+      remove_index :people, :uuid
+      remove_column :people, :uuid
+    end
+  end
+
+  private
+
+  def with_people_fk_suspended
+    if sqlite?
+      execute "PRAGMA foreign_keys = OFF"
+      yield
+    else
+      connection.disable_referential_integrity { yield }
+    end
+  ensure
+    execute "PRAGMA foreign_keys = ON" if sqlite?
+  end
+
+  def sqlite?
+    connection.adapter_name.casecmp("SQLite").zero?
   end
 
   class Person < ApplicationRecord
