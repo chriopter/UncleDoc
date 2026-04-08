8b07a705aec2be69a19789306648f065c8b1755e
chriopter <82179548+chriopter@users.noreply.github.com>
2026-04-08 11:24:52 +0200

    adjust ui

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,6 @@ Rails.application.routes.draw do
   scope "/:person_slug", constraints: { person_slug: /(?!settings|up|manifest|service-worker|people)[^\/]+/ } do
     root "dashboard#show", as: :person_root
     get "overview", to: "people#show", as: :person_overview
-    get "trends", to: "people#trends", as: :person_trends
     get "calendar", to: "dashboard#calendar", as: :person_calendar
     get "baby", to: "people#baby", as: :person_baby
     get "log", to: "dashboard#log", as: :person_log
